5fd74a8ae996bff057db2a6a7e2ef48f60f04bd6
kameamea133 <monnier1977@gmail.com>
2024-10-01 09:45:20 +0200

    login page created & stylized

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,59 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useAuth } from './../../context/AuthContext'
+import { useRouter } from 'next/navigation'
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = { email, role: 'student' };
+    login(userData);
+    router.push('/');
+  }
+
   return (
-    <div>Login page</div>
+    <div className='flex flex-col items-center justify-center h-[100vh] bg-black text-white'>
+      <h1 className='mb-6 text-2xl text-center'>Login page</h1>
+      <form 
+      className='flex flex-col items-center bg-[#A9A9A9] gap-2 p-[30px] rounded-xl shadow-md shadow-slate-700 mb-4'
+      onSubmit={handleSubmit}>
+        <input
+        type='email'
+        placeholder='Email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        className='w-[250px] border border-white bg-transparent rounded-md p-2 outline-none placeholder:text-gray-300'
+        />
+        <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        className='w-[250px] border border-white bg-transparent rounded-md p-2 outline-none placeholder:text-gray-300'
+         />
+
+        <button 
+        type='submit'
+        className='w-full p-2 bg-white text-black rounded-md cursor-pointer font-bold hover:bg-[#f0f0f0] transition duration-300'
+        >Login</button>
+      </form>
+
+      <div className='mt-6'>
+        <p>Don&apos;t have an account? <Link href='/signup' className='text-blue-700 hover:text-blue-500 transition duration-300 font-bold'>Sign up</Link></p>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default Login
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,9 +1,84 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useAuth } from '../../context/AuthContext'
+import { useRouter } from 'next/navigation'
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('student');
+  const [otp, setOtp] = useState('');
+
+  const { login } = useAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = { email, role };
+    login(userData);
+
+    router.push('/');
+  }
+
+  const handleSendOtp = () => {
+    console.log("Sending OTP...", email);
+    
+  }
+
   return (
-    <div>Sign uppage</div>
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        placeholder='Name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        />
+        
+       <div>
+        <input 
+          type="email"
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type='button' onClick={handleSendOtp}>Send OTP</button>
+       </div>
+
+       <input 
+        type="text"
+        placeholder='Enter OTP'
+        value={otp}
+        onChange={(e) => setOtp(e.target.value)}
+        required
+       />
+
+        <input 
+        type="password"
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        />
+
+        <select value={role} onChange={(e) => setRole(e.target.value)}>
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+        </select>
+        <button type='submit'>Sign Up</button>
+      </form>
+
+      <div>
+        <p>Already have an account? <Link href='/login'>Login here</Link></p>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default SignUp
